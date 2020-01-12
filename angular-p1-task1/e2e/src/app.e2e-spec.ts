7bae4509581b8a64423266bc31d80fd73057f5b0
Viktar Belski <belski.viktor.dev@gmail.com>
2020-01-13 00:29:48 +0300

    task2-layout

--- angular-p1-task1/e2e/src/app.e2e-spec.ts
+++ angular-p1-task1/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-p1 app is running!');
+    expect(page.getTitleText()).toEqual('angular-p1-task1 app is running!');
   });
 
   afterEach(async () => {
